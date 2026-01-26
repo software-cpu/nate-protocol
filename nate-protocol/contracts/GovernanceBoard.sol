c88c969e5b6eb836f2fb2ed909676c2615c9e430
Nathanael Claussen <nateclaussen@gmail.com>
2026-01-25 23:35:06 -0600

    feat: implement GovernanceBoard contract with adaptive PID controller for nonlinear adaptive control.

--- nate-protocol/contracts/GovernanceBoard.sol
+++ nate-protocol/contracts/GovernanceBoard.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 import "./StabilityEngine.sol";
 import "./TaskMarket.sol";
 
@@ -96,8 +97,12 @@ contract GovernanceBoard is Ownable {
         require(!req.executed, "Executed");
         require(!req.vetoed, "Vetoed");
         
+        console.log("ApproveMint Request:", _requestId);
+        
         // 1. Comparison Unit (Error Calculation)
         (uint256 yesPercent, ) = market.getOdds(req.linkedTaskId);
+        console.log("YesPercent:", yesPercent);
+        
         int256 confidence = int256(yesPercent);
         int256 error = confidence - targetConfidence;
         
